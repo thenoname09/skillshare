db92ee79c62ae0155bc430cfa22f34c75eb03283
Rafi <rafi.adnan365@gmail.com>
2026-05-05 13:59:30 +0600

    fix auth base urlv2

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_APP_URL
+    baseURL: process.env.BETTER_AUTH_URL
 })
